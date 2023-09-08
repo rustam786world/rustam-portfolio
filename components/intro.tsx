105fb4984490b56fd556cb4507e2fb420588ad41
Rustam Ali <rustama.ug20.ece@nitp.ac.in>
2023-09-08 22:41:25 +0530

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -39,7 +39,7 @@
                   quality={90}
                   priority={true}
   
-                className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+                className="h-30 w-30 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
              />
            </motion.div> 
   
